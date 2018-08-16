518cc91180ad3dbd5d42597a497244ba427c6391
marcuxo <marcourrutiam@gmail.com>
2018-08-16 00:00:19 -0300

    v1 calculadora c#

---
+++ Calculadora/Operadores.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculadora
+{
+    class Operadores
+    {
+        private int n1;
+        private int n2;
+
+        public Operadores(int n1, int n2)
+        {
+            this.n1 = n1;
+            this.n2 = n2;
+        }
+
+        public int Suma()
+        {
+            var resultado = n1 + n2;
+            return resultado;
+        }
+        public int Resta()
+        {
+            var resultado = n1 - n2;
+            return resultado;
+        }
+        public int Multiplicacion()
+        {
+            var resultado = n1 * n2;
+            return resultado;
+        }
+
+        public int GetDivicion()
+        {
+            var resultado = n1 / n2;
+            return resultado;
+        }
+    }
+}
---
+++ Calculadora/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculadora
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hola Mundo !!! \nDesde .Net\n");
+            Boolean cal = true;
+            while (cal)
+            {
+                Console.WriteLine("Bienvenido a la Calculadora, para salir escriba E mayuscula");
+                Console.WriteLine("Ingresa el primer numero: ");
+                int uno = int.Parse(Console.ReadLine());
+                Console.WriteLine("Ingresa la operacion a realizar(+,-,*,/): ");
+                var ope = Console.ReadLine();
+                Console.WriteLine("Ingresa el segundo numero: ");
+                int dos = int.Parse(Console.ReadLine());
+
+                Operadores oper1 = new Operadores(uno, dos);
+
+                switch (ope)
+                {
+                    case "+":
+                        Console.WriteLine("El resultado de la suma de los numeros es: " + oper1.Suma());
+                        break;
+                    case "-":
+                        Console.WriteLine("El resultado de la suma de los numeros es: " + oper1.Suma());
+                        break;
+                    case "*":
+                        Console.WriteLine("El resultado de la suma de los numeros es: " + oper1.Multiplicacion());
+                        break;
+                    case "/":
+                        Console.WriteLine("El resultado de la suma de los numeros es: " + oper1.GetDivicion());
+                        break;
+                    case "E":
+                        cal = false;
+                        break;
+                    default:
+                        Console.WriteLine("Operador no valido.");
+                        break;
+                }
+            }
+        }
+    }
+}
